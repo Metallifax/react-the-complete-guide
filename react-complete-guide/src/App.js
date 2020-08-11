7a78ae8a19b237fc45e19b719a1021cc3a8cec97
Matt Forgione <mathew_forgione@msn.com>
2020-08-11 01:55:53 -0300

    played with props and the reserved props.children reserved keyword

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -8,6 +9,16 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!!</p>
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">
+          <ul type="none">
+            ~~My Hobbies~~
+            <li>Racing</li>
+            <li>Cards</li>
+            <li>Gaming</li>
+          </ul>
+        </Person>
+        <Person name="Stephanie" age="27" />
       </div>
     );
   // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this work now?'));
